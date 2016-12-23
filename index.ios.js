ecd7db1dba9a5861707ef903920711e9560d208a
zhaitao <370985116@qq.com>
2016-12-23 15:55:21 +0800

    input

--- index.ios.js
+++ index.ios.js
@@ -9,44 +9,31 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
+    TextInput,
     View
 } from 'react-native';
 
 export default class LearnRN extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.row}>
-                    <View style={styles.item}/>
-                    <View style={styles.item}/>
-                    <View style={styles.item}/>
-                </View>
-                <View style={styles.justifyConetnt_end}>
-                    <View style={styles.item}/>
-                    <View style={styles.item}/>
-                    <View style={styles.item}/>
-                </View>
-                <View style={styles.justifyConetnt_around}>
-                    <View style={styles.item}/>
-                    <View style={styles.item}/>
-                    <View style={styles.item}/>
-                </View>
-                <View style={styles.justifyConetnt_between}>
-                    <View style={styles.item}/>
-                    <View style={styles.item}/>
-                    <View style={styles.item}/>
-                </View>
-
+                <TextInput
+                    style={{height: 40}}
+                    placeholder="Type here to translate!"
+                    onChangeText={(text) => this.setState({text})}
+                />
+                <Text style={{padding: 10, fontSize: 42}}>
+                    {this.state.text}
+                </Text>
             </View>
         );
     }
 }
-
-/*
- *  flexDirection 决定主轴方向 row→ row-reverse← column↓ column-reverse↑
- *  justifyContent 主轴排列方式 flex-start center flex-end space-around平均居中 space-between分开居中
- *  alignItems 次轴(与主轴垂直)排列方式 flex-start起始位置 flex-end终止位置 center居中 stretch展开
- */
 
 const styles = StyleSheet.create({
     container: {
@@ -55,41 +42,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF',
         paddingTop: 30
     },
-    row: {
-        backgroundColor: '#030303',
-        height: 60,
-        margin: 10,
-        flexDirection: 'row'
-    },
-
-    justifyConetnt_end: {
-        backgroundColor: '#030303',
-        height: 60,
-        margin: 10,
-        flexDirection: 'row',
-        justifyContent: 'flex-end'
-    },
-
-    justifyConetnt_around: {
-        backgroundColor: '#030303',
-        height: 60,
-        margin: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    },
-    justifyConetnt_between: {
-        backgroundColor: '#030303',
-        height: 60,
-        margin: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    item: {
-        backgroundColor: 'red',
-        height: 20,
-        margin: 5,
-        width: 20
-    }
 });
 
 AppRegistry.registerComponent('LearnRN', () => LearnRN);
